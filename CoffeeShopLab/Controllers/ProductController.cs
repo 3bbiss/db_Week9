0a7b558f48b2e970dd1d07466c05ef0f90124591
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-09-28 00:30:13 -0400

    lab stuff not done learning tho

---
+++ CoffeeShopLab/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+using CoffeeShopLab.Controllers;
+using CoffeeShopLab.Models;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var db = new MySqlConnection("Server=127.0.0.1;Database=coffeeshop;Uid=root;Pwd=abc123;");
+            List<Product> prods = db.GetAll<Product>().ToList();
+            return View(prods);
+        }
+
+        public IActionResult Detail(Product prod)
+        {
+            return View(prod);
+        }
+    }
+}
---
+++ CoffeeShopLab/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace CoffeeShopLab.Models
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int inventory { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id} {name} {description} {price} {inventory}";
+        }
+    }
+}
